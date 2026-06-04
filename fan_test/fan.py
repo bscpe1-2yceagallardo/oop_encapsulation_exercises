24c8cdafc7915f6e1038c58cb3cb2b055b126640
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-06-04 12:17:17 +0800

    uploaded the private data fields

--- fan_test/fan.py
+++ fan_test/fan.py
@@ -4,5 +4,12 @@ class Fan:
     SPEED_FAST = 3
     SPEED_VERY_FAST = 4
 
-def __init__(self, rotation_speed=SPEED_SLOW, blade_radius=5, chassis_color="blue", is_powered_on=False,
+    def __init__(self, rotation_speed=SPEED_SLOW, blade_radius=5, chassis_color="blue", is_powered_on=False,
              target_temperature=24, is_oscillating=False, active_timer_minutes=0):
+             self.__rotation_speed = rotation_speed
+             self.__blade_radius = blade_radius
+             self.__chassis_colors = chassis_color
+             self.__is_powered_on = is_powered_on
+             self.__target_temperature = target_temperature
+             self.__is_oscillating = is_oscillating
+             self.__active_timer_minutes = active_timer_minutes
