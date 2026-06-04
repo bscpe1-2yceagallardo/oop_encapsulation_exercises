4ad3fce9ffc6ca51435ea539986d00bf9e8d8066
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-06-04 14:51:56 +0800

    uploaded the object: using setters to store data inside safely

--- pet_info/pet_tester.py
+++ pet_info/pet_tester.py
@@ -9,3 +9,13 @@ def main():
     gender = input("Gender: ")
     color = input("Color: ")
     vaccinated = input("Are vaccinations complete? (yes/no): ")
+
+    # OBJECTS
+    my_pet = Pet()
+    my_pet.set_name(name)
+    my_pet.set_animal_type(animal_type)
+    my_pet.set_age(age)
+    my_pet.set_breed(breed)
+    my_pet.set_color(color)
+    my_pet.set_gender(gender)
+    my_pet.set_vaccinated(vaccinated)
