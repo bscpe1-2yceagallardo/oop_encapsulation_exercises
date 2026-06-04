32ce5483139dd5f4cf8b181b9c8daa9dc411ad57
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-06-04 12:13:23 +0800

    uploaded the function for datas

--- fan_test/fan.py
+++ fan_test/fan.py
@@ -3,3 +3,6 @@ class Fan:
     SPEED_MEDIUM = 2
     SPEED_FAST = 3
     SPEED_VERY_FAST = 4
+
+def __init__(self, rotation_speed=SPEED_SLOW, blade_radius=5, chassis_color="blue", is_powered_on=False,
+             target_temperature=24, is_oscillating=False, active_timer_minutes=0):
