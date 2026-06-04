b5180304c44b69c6454fc4df14576e5affd4bdec
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-06-04 14:44:50 +0800

    uploaded the dashboard: pet information

---
+++ pet_info/pet_tester.py
@@ -0,0 +1,11 @@
+from pet_record import Pet
+
+def main():
+    print("--- PET INFORMATION ---")
+    name = input("Pet Name: ")
+    animal_type = input("Animal Type (ex: Dog, Cat): ")
+    breed = input("Breed: ")
+    age = input("Age: ")
+    gender = input("Gender: ")
+    color = input("Color: ")
+    vaccinated = input("Are vaccinations complete? (yes/no): ")
