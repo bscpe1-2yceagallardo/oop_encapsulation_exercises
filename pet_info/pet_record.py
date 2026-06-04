29524fc0d109166d84dff968db13d6babdb21974
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-06-04 14:13:35 +0800

    uploaded the function pet data fields

--- pet_info/pet_record.py
+++ pet_info/pet_record.py
@@ -1,2 +1,9 @@
 class Pet:
     def __init__(self):
+        self.__name = ""
+        self.__animal_type = ""
+        self.__age = 0
+        self.__breed = ""
+        self.__color = ""
+        self.__gender = ""
+        self.__vaccinated = ""
