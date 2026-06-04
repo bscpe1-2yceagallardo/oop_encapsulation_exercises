4abebbd80d3d4ab06cbdb191ed9ac7b34191a860
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-06-04 13:12:16 +0800

    upload function for car datas fields

--- car_simulator/car_info.py
+++ car_simulator/car_info.py
@@ -1,2 +1,6 @@
 class Car:
     def __init__(self, year_model: int, make: str):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+        self.__fuel = 100.0
