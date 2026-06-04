4b49833d268a914aab221fc762018b13099b86f2
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-06-04 13:43:38 +0800

    added the function of securing user input with error handling

--- car_simulator/car_test.py
+++ car_simulator/car_test.py
@@ -10,3 +10,16 @@ def display_dashboard(car: Car, action: str):
     print(f"  Fuel Level  : {car.get_fuel}%")
     print(f"  Eco Rating  : {car.get_efficiency}")
     print("\n" + "=" * 40)
+
+def main():
+    print("--- CAR SIMULATOR ---")
+
+    while True:
+        try:
+            year = int(input("Enter Car Year Model (ex: 2026): "))
+            if year < 1886:
+                print("Cars didn't exist back then! Try again.")
+                continue
+            break
+        except ValueError:
+            print("Invalid Input. Please enter numeric year.")
